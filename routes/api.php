cae181f741e3c5518dbd059cbc873dc8849a866e
YoussefLMT <mcyoussef324@gmail.com>
2022-09-06 19:43:53 +0100

    add deleteProduct function

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::post('add-product', [ProductController::class, 'addProduct']);
 Route::get('get-products', [ProductController::class, 'getProducts']);
 Route::get('get-product/{id}', [ProductController::class, 'getProduct']);
 Route::put('update-product/{id}', [ProductController::class, 'updateProduct']);
+Route::delete('delete-product/{id}', [ProductController::class, 'deleteProduct']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
