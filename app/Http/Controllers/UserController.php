60ea542d4194a8f672edbb945fad5f274e8bf97b
YoussefLMT <mcyoussef324@gmail.com>
2022-09-10 12:58:58 +0100

    add user CRUD functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,148 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+
+    public function getUsers()
+    {
+        $users = User::all();
+
+        return response()->json([
+            'status' => 200,
+            'users' => $users,
+        ]);
+    }
+
+
+    
+    public function addUser(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+            'email' => 'required|email|unique:user,email',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
+
+        if($validator->fails()){
+
+            return response()->json([
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            User::create([
+                'name'=> $request->name,
+                'email'=> $request->email,
+                'password' => $request->passowrd,
+                'role' => $request->role
+            ]);
+    
+            return response()->json([
+                'status' => 200,
+                'message' => "User added successfully",
+            ]);
+        }
+    }
+
+
+
+    public function getUser($id)
+    {
+        $user = User::find($id);
+
+        if($user){
+
+            return response()->json([
+                'status' => 200,
+                'user' => $user,
+            ]);
+
+        }else{
+
+            return response()->json([
+                'status' => 404,
+                'message' => 'User not found!',
+            ]);
+
+        }
+    }
+
+
+
+    public function updateUser(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'=> 'required',
+            'email' => 'required|email|unique:user,email',
+            'role' => 'required'
+        ]);
+
+
+        if($validator->fails()){
+
+            return response()->json([
+                'status' => 422,
+                'validation_err' => $validator->messages(),
+            ]);
+
+        }else{
+
+            $user = User::find($id);
+
+            if($user){
+
+                $user->name = $request->name;
+                $user->email = $request->email;
+                if(!empty($request->password)){
+                   $user->password = Hash::make($request->password);
+                }
+                $user->role = $request->role;
+                $user->save();
+        
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Updated successully',
+                ]);
+
+            }else{
+
+                return response()->json([
+                    'status' => 404,
+                    'message' => 'Customer not found!',
+                ]);
+
+            }
+        }
+    }
+
+
+
+    public function deleteUser($id)
+    {
+        $user = User::find($id);
+
+        if($user){
+
+            $user->delete();
+    
+            return response()->json([
+                'status' => 200,
+                'message' => 'Deleted successfully',
+            ]);
+
+        }else{
+
+            return response()->json([
+                'status' => 404,
+                'message' => 'User not found!',
+            ]);
+
+        }
+    }
 }
