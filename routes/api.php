2a40d8a733bc91a5f33a09ded677e6e79d2b81a2
YoussefLMT <mcyoussef324@gmail.com>
2022-09-14 19:48:55 +0100

    some modifications

--- routes/api.php
+++ routes/api.php
@@ -49,7 +49,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('place-order', [OrderController::class, 'placeOrder']);
     Route::get('total-price', [OrderController::class, 'getOrderTotalPrice']);
-    Route::get('get-orders', [OrderController::class, 'getUserOrders']);
+    Route::get('get-user-orders', [OrderController::class, 'getUserOrders']);
 
 
 });
